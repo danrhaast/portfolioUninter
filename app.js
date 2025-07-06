08e26f555ff12af2bb7c1a9c802b74d567a6020a
danrhaast <daniel99insanos@gmail.com>
2025-07-06 12:51:33 -0300

    2.2

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-const express = require('express')
+// Definição da constante express para utilização do express
+const express = require('express') 
+
+// Definição da constante app utilizando o express
 const app = express()
+
+// constante path requirindo o path para renderizar
+// posteriormente o html do index.html
 const path = require('path')
 
+// Utilizando a constante app para definir
+// definir uma pasta estática chamada
+// front onde o app puxará todos os
+// arquivos estáticos como .html e .css
 app.use(express.static('front'))
 
+// Criação da rota principal do sistema
+// que renderiza o index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'front', 'index.html'));
 })
 
-function enviarWhats (event) {
-    event.preventDefault()
-
-    const nome = document.getElementById('nome').value
-    const email = document.getElementById('email').value
-    const msg = document.getElementById('mensagem').value
-    const telefone = '555492029984'
-
-    const texto = `Olá me chamo ${nome}, ${msg}, este é o meu email ${email}`
-    const msgformt = encodeURIComponent(texto)
-
-    const url = `https://wa.me/${telefone}?text=${msgformt}`
-
-    window.open(url, '_blank')
-    console.log('Aplicação bem sucedida')
-}
-
+// Utilizando o app para definir também o
+// servidor que irá rodar na porta 8000
+// assim temos uma pequena construção
+// back-end da página web.
 app.listen(8000, function() {
     console.log('Servidor rodando na porta: 8000')
 })
